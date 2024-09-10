2cfc0a74417bd5ce53d756ae93deb3e5e9774355
adamarArcane <golywoper@gmail.com>
2024-09-10 17:42:25 -0400

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from window import Window
+
+def main():
+    win = Window(800,600)
+    win.wait_for_close()
+
+main()
---
+++ src/window.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__canvas = Canvas()
+        self.__canvas.pack()
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+
+    def close(self):
+        self.__running = False
